493701aba79e9fa70478851b02cce7653d0ed396
MohamedYashar <arrafathyas@gmail.com>
2023-08-27 12:13:15 +0530

    Dice Task

---
+++ script.js
@@ -0,0 +1,84 @@
+const player1 = {
+    id: "player1",
+    score: 0,
+    scoreDisplay: document.getElementById("score1"),
+    rollBtn: document.getElementById("roll1"),
+  };
+  
+  const player2 = {
+    id: "player2",
+    score: 0,
+    scoreDisplay: document.getElementById("score2"),
+    rollBtn: document.getElementById("roll2"),
+  };
+
+
+  const diceImages = {
+    1: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Alea_1.png",
+    2: "https://upload.wikimedia.org/wikipedia/commons/b/b8/Alea_2.png",
+    3: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Alea_3.png",
+    4: "https://upload.wikimedia.org/wikipedia/commons/8/8d/Alea_4.png",
+    5: "https://upload.wikimedia.org/wikipedia/commons/5/55/Alea_5.png",
+    6: "https://upload.wikimedia.org/wikipedia/commons/f/f4/Alea_6.png",
+  };
+   
+   
+  const resetButton = document.getElementById("reset");
+  let currentPlayer = player1;
+
+  function rollDice() {
+    const diceValue = Math.floor(Math.random() * 6) + 1;
+    const diceImage = diceImages[diceValue];
+    const diceElement = document.getElementById(currentPlayer.id === "player1" ? "dice1" : "dice2");
+    
+    diceElement.src = diceImage;
+    
+    currentPlayer.score += diceValue;
+    currentPlayer.scoreDisplay.textContent = currentPlayer.score;
+  
+    if (currentPlayer.score >= 30) {
+        currentPlayer.rollBtn.disabled = true;
+        resetButton.disabled = false;
+        const winnerMessage = `${currentPlayer.id} wins!`;
+        const winnerMessageElement = document.createElement("b");
+        winnerMessageElement.style.background = "green";
+        winnerMessageElement.style.height = "25px";
+        winnerMessageElement.textContent = winnerMessage;
+        currentPlayer.scoreDisplay.parentElement.appendChild(winnerMessageElement);
+        console.log(winnerMessage);
+      }else {
+      currentPlayer.rollBtn.disabled = true;
+      toggleCurrentPlayer();
+    }
+  }
+  
+
+//    to toggle the palyer after each time after clicking a button
+  
+  function toggleCurrentPlayer() {
+    currentPlayer.rollBtn.disabled = true;
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+    currentPlayer.rollBtn.disabled = false;
+  }
+
+//   reset the game
+  
+  function resetGame() {
+    player1.score = 0;
+    player2.score = 0;
+    player1.scoreDisplay.textContent = "0";
+    player2.scoreDisplay.textContent = "0";
+    player1.rollBtn.disabled = false;
+    player2.rollBtn.disabled = true;
+    resetButton.disabled = true;
+    currentPlayer = player1;
+    const diceImage = diceImages[diceValue];
+
+  }
+  
+  player1.rollBtn.addEventListener("click", rollDice);
+  player2.rollBtn.addEventListener("click", rollDice);
+  resetButton.addEventListener("click", resetGame);
+  
+  resetGame(); // Initialize game state
+  
